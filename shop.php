137a25bc7abd2fb4fa136b381287aa9b4a649f1f
Li Xintong <lixintong@LIXINTONGdeMacBook-6.local>
2019-10-23 09:56:38 +0800

    add database

--- shop.php
+++ shop.php
@@ -18,108 +18,106 @@
 	<div class="wrapper">
 		<?php
 		// session_start();
-		// $con = new mysqli("");
-		// if($con->connection_error){
-		// 	include_once("error.php");
-		// 	exit();
-		// }
+		$conn = new mysqli("localhost","f34ee","Fukua971005");
+		if($conn->connection_error){
+			include_once("error.php");
+			exit();
+		}
 		include 'header.php';
 		
-        $type= $_GET['type'];
+        
 
-        $pageNum=0;
-        foreach($_GET as $param_name => $param_value){
-        	if($param_name == 'type'){
-        		$cond = "";
-        		foreach($param_value as $category){
-        			$cond = $cond . $param_name . '="'. $category .'")';
+        // $pageNum=0;
+        // foreach($_GET as $param_name => $param_value){
+        // 	if($param_name == 'type'){
+        // 		$cond = "";
+        // 		foreach($param_value as $category){
+        // 			$cond = $cond . $param_name . '="'. $category .'")';
 
-        		}
+        // 		}
 
-        	}
-        }
+        // 	}
+        // }
+        ?>
         
 
 
-        echo '
-        <div class="slide-container">
-		    <div class="slide">
-		      <img src="img/slide1.jpg" style="width:100%">
-		    </div>
-		    <div class="slide">
-		      <img src="img/slide2.jpg" style="width:100%">
-		    </div>
-		    <div class="slide">
-		      <img src="img/slide3.jpg" style="width:100%">
-		    </div>
-		  </div>
-		  <br>
-        ';
+       
+<!-- '<div class="slide-container">
+    <div class="slide">
+      <img src="img/slide1.jpg" style="width:100%">
+    </div>
+    <div class="slide">
+      <img src="img/slide2.jpg" style="width:100%">
+    </div>
+    <div class="slide">
+      <img src="img/slide3.jpg" style="width:100%">
+    </div>
+  </div>
+  <br>
+        ' -->
 
-        echo'
-         <div class="filterbar"  style="width:50%; margin-left: auto;margin-right: auto; float: left;">
-		<div class="custom-select filter">     
-		      <select id="color" >
-		        <option value="0">color</option>
-		        <option value="1">Pink</option>
-		        <option value="2">Blue</option>
-		        <option value="3">Red</option>
-		        <option value="4">Multicolor</option>
-		      </select>
-		 </div>
-		 <div class="custom-select filter" >     
-		      <select id="price">
-		      <option value="0">price</option>
-		      <option value="1">0-50</option>
-		      <option value="2">50-100</option>
-		      <option value="3">100-150</option>
-		      <option value="4">150-200</option>
-		      <option value="5">200+</option>
-		    </select>
-		 </div>
-		 <div class="custom-select filter" >     
-		      <select id="theme" >
-		        <option value="0">theme</option>
-		        <option value="1">Career Aspiration</option>
-		        <option value="2">Animals &amp; Pets</option>
-		        <option value="3">Firytale</option>
-		        <option value="4">Flower &amp; nature</option>
-		        <option value="5">Love &amp; Romance</option>
-		      </select>
-		 </div>
-		</div>
+      
+<div class="filterbar"  style="width:50%; margin-left: auto;margin-right: auto; float: left;">
+<div class="custom-select filter">     
+      <select id="color" >
+        <option value="0">color</option>
+        <option value="1">Pink</option>
+        <option value="2">Blue</option>
+        <option value="3">Red</option>
+        <option value="4">Multicolor</option>
+      </select>
+ </div>
+ <div class="custom-select filter" >     
+      <select id="price">
+      <option value="0">price</option>
+      <option value="1">0-50</option>
+      <option value="2">50-100</option>
+      <option value="3">100-150</option>
+      <option value="4">150-200</option>
+      <option value="5">200+</option>
+    </select>
+ </div>
+ <div class="custom-select filter" >     
+      <select id="theme" >
+        <option value="0">theme</option>
+        <option value="1">Career Aspiration</option>
+        <option value="2">Animals &amp; Pets</option>
+        <option value="3">Firytale</option>
+        <option value="4">Flower &amp; nature</option>
+        <option value="5">Love &amp; Romance</option>
+      </select>
+ </div>
+</div>
 
-        ';
-
-        $query = 'SELECT FROM products WHERE';
+       <?php
+        $type= $_GET['type'];
+        $query = "SELECT * FROM products WHERE category=".$type.";";
         $result = $conn->query($query);
-        if($result){
-        	$num_rows = $result->num_rows;
-        	if($num_rows>0){
-        		echo '<div class="row">'
-        		for($i=0; $i<num_rows;$i++){
-        			$row = $result.fetch_assoc();
-	        		$product_id=$row["id"];
-	        		$product_name = stripslashes($row["name"]);
-	        		$product_price = $row["price"];
-	        		$discount = $row['discount'];
-	        		echo "<div class="3cols">";
-	        		include 'product.php';
-	        		echo "</div>";
+        echo $result;
+        // if($result){
+        // 	$num_rows = $result->num_rows;
+        // 	if($num_rows>0){
+        // 		echo '<div class="row">';
+        // 		echo $result;
+        		// for($i=0; $i<num_rows;$i++){
+        		// 	$row = $result->fetch_assoc();
+        		// 	echo($row)
+	        	// 	$product_id=$row["id"];
+	        	// 	$product_name = stripslashes($row["name"]);
+	        	// 	$product_price = $row["price"];
+	        	// 	$discount = $row['discount'];
+	        	// 	echo "<div class="3cols">";
+	        	// 	include 'product.php';
+	        	// 	echo "</div>";
 
-        		}
-        		$result->free();
-        		echo "</div>";
-
-
-        		
+        		// }
+        		// $result->free();
+        		// echo "</div>";
+        	//}
         		?>
 
-        	}
-        }
-
-        ?>
-
+        
 
 
 <script type="text/javascript" src='javascript/slide.js'></script>
---
+++ sql/init.sql
@@ -0,0 +1,301 @@
+USE f34ee;
+
+CREATE TABLE customers (
+  id int unsigned auto_increment primary key,
+  first_name  varchar(255) not null,
+  last_name  varchar(255) not null,
+  email  varchar(255) not null,
+  password  varchar(255) not null,
+  dateofbirth  date
+);
+
+CREATE TABLE products  (
+   id  int unsigned primary key,
+   name  varchar(255) not null,
+   category  varchar(255) not null,
+   sub_category  varchar(255) not null,
+   price  float not null,
+   detail  LONGTEXT not null,
+   stock  int not null,
+   color  varchar(255) not null,
+   theme  varchar(255) not null,
+   image1  varchar(255) not null,
+   image2  varchar(255) not null
+);
+
+CREATE TABLE  t_order  (
+   id  int unsigned  primary key,
+   customer_id  int not null,
+   date_order_placed  datetime not null,
+   total_price  float not null
+);
+
+CREATE TABLE  order_items  (
+   id  int unsigned auto_increment primary key,
+   product_id  int not null,
+   order_id  int not null,
+   order_item_quantity  int not null,
+   order_item_price  float not null
+);
+
+CREATE TABLE discount(
+  id  int unsigned auto_increment primary key,
+  product_id int not null,
+  discount float not null
+
+);
+
+CREATE TABLE bank_simulation (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  type BIT(1) NOT NULL,
+  number VARCHAR(19) NOT NULL,
+  expiry DATE NOT NULL,
+  CVV VARCHAR(4) NOT NULL,
+  name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE banner{
+  id int unsigned auto_increment primary key,
+  img1 varchar(255) not null,
+  img2 varchar(255) not null,
+  img3 varchar(255) not null,
+  category varchar(255) not null
+}
+
+INSERT INTO customers (first_name, last_name, email, password, dateofbirth) VALUES ("John","Smith","JohnSmith@gmail.com","John1234567","1996-05-10");
+INSERT INTO customers (first_name, last_name, email, password, dateofbirth) VALUES ("Tony","Robbins","Tony123@gmail.com","Tony1234567","1960-02-19");
+INSERT INTO customers (first_name, last_name, email, password, dateofbirth) VALUES ("Sara","Bareilles","Sara@gmail.com","Sara1234567@","1980-12-21");
+
+INSERT INTO discount (product_id, discount) VALUES (1,0.9);
+INSERT INTO discount (product_id, discount) VALUES (4,0.8);
+INSERT INTO discount (product_id, discount) VALUES (5,0.7);
+
+-- INSERT INTO banner (img1,img2,img3,category) VALUES(
+
+-- )
+
+INSERT INTO products (id,name,category,sub_category,price,detail,stock,color,theme,image1,image2)
+VALUES(
+  1,
+  "Bruno the Unicorn Punk Band Dangle Charm",
+  "Charms",
+  "Dangle Charms",
+  60.00,
+  "Get the band back together by enlisting Bruno the Unicorn, the lead guitarist from The Pandoras and a true rock star.",
+  50,
+  "Multi",
+  "Hobbies & Passions",
+  "/Users/lixintong/Documents/webimgs/798320ENMX_RGB.jpeg",
+  "/Users/lixintong/Documents/webimgs/798320ENMX_ABC123_MODEL_ECOM_RGB.jpeg"
+),
+(
+2,
+"Disney Luminous Ariel Dangle Charm",
+"Charms",
+"Dangle Charms",
+75.00,
+"Ariel will become part of your world with this Disney The Little Mermaid charm. Striking a delicate pose, Ariel sits atop a freshwater cultured pearl. ",
+50,
+"Multi",
+"Fairytale",
+"/Users/lixintong/Documents/webimgs/798232CZ_RGB.jpeg",
+"/Users/lixintong/Documents/webimgs/798232CZ_ABC123_MODEL_ECOM_RGB.jpeg"
+),
+(
+3,
+"Pink Heart Labyrinth Dangle Charm",
+"Charms",
+"Dangle Charms",
+60.00,
+"Follow your heart always with this sweet token of love. Mixing openwork hearts, our signature metals and pretty pink enamel, the labyrinthian design features the words 'Follow your' and a cut-out heart.",
+50,
+"Multi",
+"Love & Romance",
+"/Users/lixintong/Documents/webimgs/787801NBP_RGB.jpeg",
+"/Users/lixintong/Documents/webimgs/787801NBP_ABC123_MODEL_ECOM_RGB.jpeg"
+),
+(
+  4,
+  "Pandora Reflexions™ Letter G Clip Charm",
+  "Charms",
+  "Clips",
+  35.00,
+  "Detailed with contrasting sides, this decorative letter G charm is hand-finished from sterling silver.",
+  50,
+  "Silver",
+  "Alphabet & Numbers",
+  "/Users/lixintong/Documents/webimgs/798203_RGB.jpeg",
+  "/Users/lixintong/Documents/webimgs/798203_ABC123_MODEL_ECOM_RGB.jpeg"
+),
+(
+  5,
+  "Lucky Four-Leaf Clover Clip Charm",
+  "Charms",
+  "Clips",
+  50.00,
+  "Wear a lasting reminder of a family that grows and blossoms with this clip charm, compatible with PANDORA Reflexions™ flat weave bracelets. The sterling silver design features a family tree motif as a precious symbol of family heritage and beloved members.",
+  50,
+  "Clear",
+  "Flowers & Nature",
+  "/Users/lixintong/Documents/webimgs/768000CZ_RGB.jpeg",
+  "/Users/lixintong/Documents/webimgs/768000CZ_V2_RGB.jpeg"
+),
+(
+6,
+"Polished Flower Clip Charm",
+"Charms",
+"Clips",
+45.00,
+"Add floral sophistication to your Pandora Reflexions bracelet with this radiant Pandora Rose clip charm. The blushing pink metal design is reversible: one side is detailed with indentations, the other is left smooth.",
+50,
+"Golden",
+"Flowers & Nature",
+"/Users/lixintong/Documents/webimgs/787897_RGB.jpeg",
+"/Users/lixintong/Documents/webimgs/787897_ABC123_MODEL_ECOM_RGB.jpeg"
+),
+(
+  7,
+  "Disney, Mickey Floating Locket, Clear CZ",
+  "Necklaces",
+  "Locket Necklaces",
+  180.00,
+  "This Mickey-mouse shaped floating locket design has been specially designed to let you celebrate you favorite Disney moments. Its sterling silver frame and clear sapphire glass walls beautifully display and protect your tiny treasures.",
+  50,
+  "Silver",
+  "Love & Romance",
+  "/Users/lixintong/Documents/webimgs/397177_RGB.jpeg",
+  "/Users/lixintong/Documents/webimgs/397177_ABC123_MODEL_eCOM_02_RGB.jpeg"
+),
+(
+8,
+"Pandora Lockets Heart Key Necklace",
+"Necklaces",
+"Locket Necklaces",
+140.00,
+"Express your personal love story with this striking key-shaped locket pendant. Crafted from sterling silver with a luxurious sapphire crystal window, the timeless design lends meaningful dimension to your personal collection of petite charms.",
+50,
+"Silver",
+"Love & Romance",
+"/Users/lixintong/Documents/webimgs/396581CZ_RGB.jpeg",
+"/Users/lixintong/Documents/webimgs/396581CZ_ABC123_MODEL_eCOM_01_RGB.jpeg"
+),
+(
+9,
+"Pandora ESSENCE Snake Chain Necklace",
+"Necklaces",
+"Locket Necklaces",
+200.00,
+"This stylish and slender ESSENCE Collection necklace is hand-finished from beautiful PANDORA Rose™. The collier length elegantly displays your charms and highlights your collarbone.",
+50,
+"Gold",
+"Flower & Nature",
+"/Users/lixintong/Documents/webimgs/387277_RGB.jpeg",
+"/Users/lixintong/Documents/webimgs/387277_ABC123_MODEL_eCOM_01_RGB.jpeg"
+),
+(
+10,
+"Chiming Filigree Hearts Pendant Necklace",
+"Necklaces",
+"Chain Necklaces",
+60.00,
+"Emitting a soft and soothing chime, this intricate PANDORA Rose™ filigree necklace promotes calm and relaxation – a welcome feeling for any woman.",
+50,
+"Gold",
+"Flower & Nature",
+"/Users/lixintong/Documents/webimgs/387299_RGB.jpeg",
+"/Users/lixintong/Documents/webimgs/387299_ABC123_MODEL_eCOM_01_RGB.jpeg"
+),
+(
+11,
+"Vintage Circle Collier Necklace",
+"Necklaces",
+"Chain Necklaces",
+125.00,
+"Enhance any neckline in a spectacular fashion with this striking geometric design in PANDORA Rose™. The necklace's shape and dazzling cubic zirconia stone embellishments will nestle elegantly against your collarbones",
+50,
+"Gold",
+"Flower & Nature",
+"/Users/lixintong/Documents/webimgs/380523CZ_RGB.jpeg",
+"/Users/lixintong/Documents/webimgs/380523CZ_ABC123_MODEL_eCOM_01_RGB.jpeg"
+),
+(
+12,
+"Crown & Interwined Hearts Pendant Necklace",
+"Necklaces",
+"Chain Necklaces",
+90.00,
+"Complete your outfit with a crown. This sterling silver necklace creates a modern fairy tale feel with united hearts and their crowning glory. Wear it in three different lengths to suit your look.",
+50,
+"Silver",
+"Love & Romance",
+"/Users/lixintong/Documents/webimgs/397719_RGB.jpeg",
+"/Users/lixintong/Documents/webimgs/397719_ABC123_MODEL_eCOM_01_RGB.jpeg"
+),
+(
+  13,
+  "Sparkling Wishbone Ring",
+  "Rings",
+  "Promise",
+  55.00,
+  "Make all your wishes come true with this delicate Pandora Rose™ ring, lined with sparkling cubic zirconia stones. Simple yet elegant, this wishbone-shaped ring can be worn on its own as a shimmering statement, or paired with other Pandora stackable rings to create a unique, highly individual look.",
+  50,
+  "Clear",
+  "Flowers & Nature",
+  "/Users/lixintong/Documents/webimgs/186316CZ_RGB.jpeg",
+  "/Users/lixintong/Documents/webimgs/186316CZ_ABC123_MODEL_eCOM_01_RGB.jpeg"
+),
+(
+14,
+"Round Sparkle Halo Ring",
+"Rings",
+"Promise",
+90.00,
+"Exude timeless glamor with this opulent cocktail ring. Finely crafted in sterling silver, this vintage-inspired piece features a large brilliant-cut cubic zirconia stone surrounded by tiny shimmering stones. This sparkling Pandora ring will add sophistication to every outfit, from simple, everyday ensembles to glamorous evening wear.",
+50,
+"Clear",
+"",
+"/Users/lixintong/Documents/webimgs/196250CZ_RGB.jpeg",
+"/Users/lixintong/Documents/webimgs/196250CZ_MODEL_eCom_01_RGB.jpeg"
+),
+(
+  15,
+  "Square Sparkle Halo Ring",
+  "Rings",
+  "Promise",
+  120.00,
+  "This stunning vintage-inspired ring is one you'll treasure forever. Crafted in Pandora Rose™ ",
+  50,
+  "Clear",
+  "",
+  "/Users/lixintong/Documents/webimgs/180947CZ_RGB.jpeg",
+  "/Users/lixintong/Documents/webimgs/180947CZ_V2_RGB.jpeg"
+
+),
+(
+  16,
+  "Princess Wishbone Ring",
+  "Rings",
+  "stackable",
+  170.00,
+  "Our popular wishbone ring gets a royal makeover with the princess wishbone ring in Pandora Rose™ featuring a tiara design. ",
+  50,
+  "Clear",
+  "",
+  "/Users/lixintong/Documents/webimgs/187736CZ_RGB.jpeg",
+  "/Users/lixintong/Documents/webimgs/187736CZ_ABC123_MODEL_ECOM_RGB.jpeg"
+),
+(
+  17,
+  "Tiara Wishbone Ring",
+  "Rings",
+  "stackable",
+  130.00,
+  "Turn heads wherever you go with this Tiara Wishbone ring hand-finished in smooth and polished sterling silver. The stackable wishbone ring features five claw-set clear cubic zirconia that form a delicate tiara crown on top of the ring. It's perfect worn by itself or can be used to create more drama when stacked with wishbone rings in mixed metals, like sterling silver and Pandora Shine™ (18k gold-plated sterling silver)",
+  50,
+  "Clear",
+  "",
+  "/Users/lixintong/Documents/webimgs/198282CZ_RGB.jpeg",
+  "/Users/lixintong/Documents/webimgs/198282CZ_ABC123_MODEL_ECOM_RGB.jpeg"
+);
+
+
